8f710b9ac8ba36ee14c5da5385bf0eb4adf394fc
Gokul06011996 <117106173+Gokul06011996@users.noreply.github.com>
2022-12-21 18:24:24 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,28 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Angular';
 
-  a = [7, 2, 20, 28, 1, 3, 5, 19, 29, 12];
+  array = [7, 2, 20, 28, 1, 3, 5, 19, 29, 12];
 
   ngOnInit() {
-    for (let i = 0; i < this.a.length; i++) {
-      for (let j = i; j < this.a.length; j++) {
-        if (this.a[i] > this.a[j]) {
-          let temp = this.a[i];
-          this.a[i] = this.a[j];
-          this.a[j] = temp;
+    for (let i = 0; i < this.array.length; i++) {
+      for (let j = i; j < this.array.length; j++) {
+        if (this.array[i] > this.array[j]) {
+          let temp = this.array[i];
+          this.array[i] = this.array[j];
+          this.array[j] = temp;
         }
       }
     }
-    console.log('Ascending',this.a);
-    for (let i = 0; i < this.a.length; i++) {
-      for (let j = i; j < this.a.length; j++) {
-        if (this.a[i] < this.a[j]) {
-          let temp = this.a[i];
-          this.a[i] = this.a[j];
-          this.a[j] = temp;
+    console.log('arrayscending', this.array);
+    for (let i = 0; i < this.array.length; i++) {
+      for (let j = i; j < this.array.length; j++) {
+        if (this.array[i] < this.array[j]) {
+          let temp = this.array[i];
+          this.array[i] = this.array[j];
+          this.array[j] = temp;
         }
       }
     }
-    console.log('Descending',this.a);
+    console.log('Descending', this.array);
   }
 }
